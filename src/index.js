8438eb7efa31133207dbd407055a90e4306fcea6
Michael Duberstein <dubermic@flutterint.com>
2022-07-07 18:45:46 -0400

    JSX: After Clip 19 Applying sytling in JSX, rules, after Rule#5

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App () { 
-  return <input spellcheck={false} />
+  return <input style= {{ border: "5px solid blue"}} />
 }
 
 // 5) Show the componenet on the screen
