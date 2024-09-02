9216b22610bb9d3fa7884c2c4b02b357e8bdf05d
Michael Duberstein <mduberstein@gmail.com>
2024-09-02 17:52:01 -0400

    After Clip 21-2024 Extracting Components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // 1) Import the React and ReactDom Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -8,10 +9,11 @@ const el = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-function App () { 
-  return <input placeholder="Hi there!" />
-}
+// // 4) Create a component
+// Moved to the separate file App.js
+// function App () { 
+//   return <input placeholder="hi there"/>
+// }
 
 // 5) Show the componenet on the screen
 root.render(<App />);
